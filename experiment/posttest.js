ce4b8440925a4ee8eeb1218ff7d2efe3523b9278
Hritiksinghal01 <69353951+Hritiksinghal01@users.noreply.github.com>
2020-08-09 14:04:01 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -90,28 +90,62 @@
 
   const myQuestions = [
     {
-      question: "This is question 1?",  ///// Write the question inside double quotes
+      question: "Velocity within the given fields would be similar in magnitude, direction and turbulence pattern when ",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "Nusselt number are different",                  ///// Write the option 1 inside double quotes
+        b: "Nusselt number are same",                  ///// Write the option 2 inside double quotes
+        c: "Reynolds number are different",                  ///// Write the option 3 inside double quotes
+        d: "Reynolds number are same"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
+      correctAnswer: "d"                ///// Write the correct option inside double quotes
     },
 
     {
-      question: "This is question 2?",  ///// Write the question inside double quotes
+      question: "A pipe has a diameter of 0.2 m in which a fluid flows with a velocity of 0.3 m3/s. Determine whether the flow is laminar or turbulent calculating the Reynolds number. Assume kinematic viscosity = 0.5 × 10-4 m2 /s?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "The flow is laminar having Reynolds number 1200",                  ///// Write the option 1 inside double quotes
+        b: "The flow is laminar having Reynolds number 2100",                  ///// Write the option 2 inside double quotes
+        c: "The flow is laminar having Reynolds number 2200",                  ///// Write the option 3 inside double quotes
+        d: "The flow is neither laminar nor turbulent"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "b"                ///// Write the correct option inside double quotes
+      correctAnswer: "a"                ///// Write the correct option inside double quotes
     },                                  ///// To add more questions, copy the section below 
-    									                  ///// this line
+    
+    	{
+      question: "What is the appropriate definition of streamflow?",
+      answers: {
+        a: "The way in which water moves within a stream channel",
+        b: "The direction of the flow of the water within a stream",
+        c: "The velocity of the water within a stream",
+        d: "The amount of water within a stream"
+      },
+      correctAnswer: "c"
+    },
+    
+    {
+      question: "Which of the following is NOT a characteristic of a stream with laminar streamflow?",
+      answers: {
+        a: "Quiet",
+        b: "Water that moves quickly",
+        c: "Water that flows in a straight line",
+        d: "No visible physical barriers"
+      },
+      correctAnswer: "b"
+    },
+    
+    {
+      question: "The turbulent flow has ?",
+      answers: {
+        a: "streakline motion",
+        b: "parabolic velocity distribution",
+        c: "random orientation of fluid particles",
+        d: "small slope of velocity profile at the wall"
+      },
+      correctAnswer: "c"
+    },
 
+    ///// this line
+ 
 
     /* To add more MCQ's, copy the below section, starting from open curly braces ( { )
         till closing curly braces comma ( }, )
